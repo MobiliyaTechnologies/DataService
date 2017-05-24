ebe630ec1606c8bd6eb487ec1d23b81cae8b07e9
Ketan Kakade <ketan.kakade@mobiliya.com>
2017-05-24 13:00:13 +0530

    Added TestCase1

---
+++ TestCases/TestCase1/DataServiceTestCase1/DataService/DataService/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DataService
+{
+    public partial class DataService : ServiceBase
+    {
+        
+        public DataService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
